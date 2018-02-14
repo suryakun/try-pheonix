5aa7e049d7d93b0f4be2c77c1c106ede85a2b3a6
surya <surya.ramshere@gmail.com>
2018-02-15 00:18:08 +0700

    Add react to brunch

--- platform/assets/brunch-config.js
+++ platform/assets/brunch-config.js
@@ -46,7 +46,8 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
+      ignore: [/web\/static\/vendor/],
+      presets: ["es2015", "react"]
     }
   },
 
@@ -57,6 +58,7 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    whitelist: ["pheonix", "pheonix_html", "react", "react-dom","redux", "react-redux"]
   }
 };
--- platform/assets/js/app.js
+++ platform/assets/js/app.js
@@ -19,3 +19,19 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+
+let store = createStore(rootReducer)
+
+render (
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
